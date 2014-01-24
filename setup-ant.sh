2b37818c8031c2b1d9ca03c3927cd924dca19524
n8fr8 <nathan@freitas.net>
2014-01-23 22:40:46 -0500

    modified to enable command line building from ant

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+target="android-18"
+
 for f in `find external/ -name project.properties`; do
-    android update lib-project -p `dirname $f`
+    android update lib-project -t $target -p `dirname $f`
 done
-android update project -p app/ --subprojects
+android update project -t $target -p app/ --subprojects
+
+cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
+cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/CacheWord/cachewordlib/libs/android-support-v4.jar
+cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
+
