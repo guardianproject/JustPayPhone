b9ec94c67ad96b6bd5d23b7d44f30787edf94faa
n8fr8 <nathan@freitas.net>
2014-02-07 09:10:18 -0500

    update setup script

--- setup-ant.sh
+++ setup-ant.sh
@@ -5,6 +5,7 @@ target="android-18"
 for f in `find external/ -name project.properties`; do
     android update lib-project -t $target -p `dirname $f`
 done
+
 android update project -t $target -p app/ --subprojects
 
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libonionkit/libs/android-support-v4.jar
